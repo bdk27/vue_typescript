827baeb939172f1794d87b68f4d0832b5ffdb106
YCheng <briansam195@gmail.com>
2025-02-19 21:36:02 +0800

    新增vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
+import store from "./store";
+import router from "./router";
 import "./index.css";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 
-const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia);
+app.use(store);
+app.use(router);
 app.mount("#app");
